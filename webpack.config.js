db879ca1e6e09199f9ea78a2a68f8d2195c88c3d
Komarichev Serg <komarichev@gmail.com>
2017-06-19 14:20:08 +0300

    second commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+ // input
+ 	entry: './src'
+ // output
+ output: {
+ 	path: __dirname + '/build'
+ }
+ // transformations
+ // sourcemaps
+ // server
+
+
+}
