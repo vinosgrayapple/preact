c03e51ff03c95284eb21b013f915296e53bf8024
Komarichev Serg <komarichev@gmail.com>
2017-06-19 14:37:58 +0300

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,34 @@ const path = require('path');
 
 module.exports = {
  // input
- 	entry: './src'
+ entry: './src',
  // output
  output: {
- 	path: __dirname + '/build'
- }
+ 	path: path.join(__dirname, 'build'),
+ 	filename: 'bundle.js'
+ },
  // transformations
+ module: {
+ 	rules: [
+ 		{
+ 			test: /|.jsx?/i,
+ 			loader: 'babel-loader',
+ 			options: {
+ 				presets: ['env'],
+ 				plugins: [
+ 				['transform-react-jsx', {pragma: 'h'}]
+ 				]
+ 			}
+ 		}
+ 	]
+ },
  // sourcemaps
+ devtool: 'source-map',
  // server
-
+devServer: {
+	contentBase: path.join(__dirname, 'src'),
+	compress: true,
+	historyApiFallback: true
+}
 
 }
